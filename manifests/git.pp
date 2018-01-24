2dc7e42ee47d334f7d614e9451368d5daee1f875
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-01-24 15:57:43 +0100

    addition of subversion package addition of excludes for svn co

--- manifests/git.pp
+++ manifests/git.pp
@@ -1,9 +1,18 @@
 class rtm::git (
     String $git_clone_directory = '/root/tmp/rtm/sources'
 ) {
+    package { 'subversion': ensure => 'installed' }
+
     vcsrepo { $git_clone_directory:
         ensure   => present,
         provider => svn,
         source   => 'http://ifn-dev.ign.fr/svn/RTM/trunk',
+        excludes => [
+          'documentation/',
+          'Vagrantfile',
+          '.buildpath',
+          '.project',
+          'README.txt',
+        ]
     }
 }
