d56d5ab6f770b3009b1e985d1da0b117dfb20733
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-01-24 16:38:32 +0100

    addition of svn includes

--- manifests/git.pp
+++ manifests/git.pp
@@ -7,12 +7,25 @@ class rtm::git (
         ensure   => present,
         provider => svn,
         source   => 'http://ifn-dev.ign.fr/svn/RTM/trunk',
-        /*includes => [
-          'Bases SQL/',
+        includes => [
+          'Bases SQL/0 - Create harmonized_data schema.sql',
+          'Bases SQL/0 - Create mapping schema.sql',
+          'Bases SQL/0 - Create metadata schema.sql',
+          'Bases SQL/0 - Create raw_data schema.sql',
+          'Bases SQL/0 - Create website schema.sql',
+          'Bases SQL/1 - Create user.sql',
+          'Bases SQL/2 - Populate mapping schema.sql',
+          'Bases SQL/2 - Populate metadata schema.sql',
+          'Bases SQL/2 - Populate website schema.sql',
+          'Bases SQL/3 - Right management.sql',
+          'Bases SQL/Data/',
+          'Bases SQL/Mapping/',
+          'Bases SQL/Metadata/',
+          'Bases SQL/__convert_latlon_dms2.sql',
           'mapserver/',
           'services_configs/',
           'website/',
-        ],*/
-        excludes => 'Bases SQL/donnees_fournies_par_RTM/'
+        ],
+        # the excludes parameters doesn't work with svn provider
     }
 }
