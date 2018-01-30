3e75217af16c6b8ecf50978e3dcdb54ba599cf26
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-01-30 17:42:07 +0100

    mapserv conf

--- manifests/mapserv.pp
+++ manifests/mapserv.pp
@@ -7,20 +7,21 @@ class rtm::mapserv (
     package { $enhancers: ensure => 'installed' }
 
     file { "${conf_directory}/mapserver":
-      ensure  => 'directory',
+      ensure => 'directory',
       recurse => true,
       source => "${git_clone_directory}/mapserver",
-      group   => 'www-data',
-    }
-    # mapserv is a fcgi compatible, use default config sethandler with .fcgi
-    file { '/usr/lib/cgi-bin/mapserv.fcgi':
-        ensure  => link,
-        target => '/usr/lib/cgi-bin/mapserv',
-    }
+      group => 'www-data',
+    }->
     exec { [  "sed -i 's|vrtm-onf.ifn.fr|${fqdn}|' rtm.map",
               "sed -i 's|/vagrant/ogam/website/htdocs/logs|${log_directory}|' rtm.map",
               "sed -i 's|/vagrant/ogam/mapserver|${conf_directory}/mapserver|' rtm.map" ]:
-      path     	=> '/usr/bin:/usr/sbin:/bin',
-      cwd 		  => "${conf_directory}/mapserver",
+      path => '/usr/bin:/usr/sbin:/bin',
+      cwd => "${conf_directory}/mapserver",
+    }
+
+    # mapserv is a fcgi compatible, use default config sethandler with .fcgi
+    file { '/usr/lib/cgi-bin/mapserv.fcgi':
+        ensure => link,
+        target => '/usr/lib/cgi-bin/mapserv',
     }
 }
