849f8b414ce9212e7a461e116e4922b9cae4c924
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-02-12 11:48:26 +0100

    simplification of the passage of the parameters

--- manifests/tasks.pp
+++ manifests/tasks.pp
@@ -1,43 +1,18 @@
-class rtm::tasks (
-    String $docroot_directory = '/var/www/rtm/web',
-    String $git_clone_directory = '/root/tmp/rtm/sources',
-    String $local_scripts_directory = '/root/tmp/rtm/scripts',
-    String $www_directory = '/var/www/rtm',
-    String $server_upload_directory = '/var/www/rtm/upload',
-    String $service_upload_directory = '/var/tmp/rtm/service_upload',
-    String $tomcat_directory = '/var/lib/tomcat8',
-    String $vhost_servername = 'agent.example.com',
-    String $cookie_domain = '.example.com',
-) {
+class rtm::tasks {
 
   file { "${local_scripts_directory}/build_db.sh":
     ensure  => 'file',
     mode    => '0400',
-    content => epp("${module_name}/build_db.epp", {
-      server_upload_directory => $server_upload_directory,
-      service_upload_directory => $service_upload_directory,
-      git_clone_directory => $git_clone_directory
-    }),
+    content => epp("${module_name}/build_db.epp"),
   }
   file { "${local_scripts_directory}/build_ogamserver.sh":
     ensure  => 'file',
     mode    => '0400',
-    content => epp("${module_name}/build_ogamserver.epp", {
-      docroot_directory => $docroot_directory,
-      git_clone_directory => $git_clone_directory,
-      www_directory => $www_directory,
-      server_upload_directory => $server_upload_directory,
-      vhost_servername => $vhost_servername,
-      cookie_domain => $cookie_domain,
-    }),
+    content => epp("${module_name}/build_ogamserver.epp"}),
   }
   file { "${local_scripts_directory}/build_ogamservices.sh":
     ensure  => 'file',
     mode    => '0400',
-    content => epp("${module_name}/build_ogamservices.epp"
-    /*, {
-      git_clone_directory => $git_clone_directory,
-      tomcat_directory => $tomcat_directory,
-    }*/),
+    content => epp("${module_name}/build_ogamservices.epp"),
   }
 }
