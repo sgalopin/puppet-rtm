24a78a068ada63a86186852f866b70675d795293
Sylvain Galopin <sylvain.galopin@ign.fr>
2018-02-12 11:53:29 +0100

    bugfix  Warning: Unknown variable: 'local_scripts_directory'

--- manifests/tasks.pp
+++ manifests/tasks.pp
@@ -1,16 +1,16 @@
 class rtm::tasks {
 
-  file { "${local_scripts_directory}/build_db.sh":
+  file { "${rtm::local_scripts_directory}/build_db.sh":
     ensure  => 'file',
     mode    => '0400',
     content => epp("${module_name}/build_db.epp"),
   }
-  file { "${local_scripts_directory}/build_ogamserver.sh":
+  file { "${rtm::local_scripts_directory}/build_ogamserver.sh":
     ensure  => 'file',
     mode    => '0400',
     content => epp("${module_name}/build_ogamserver.epp"),
   }
-  file { "${local_scripts_directory}/build_ogamservices.sh":
+  file { "${rtm::local_scripts_directory}/build_ogamservices.sh":
     ensure  => 'file',
     mode    => '0400',
     content => epp("${module_name}/build_ogamservices.epp"),
